fc8c2ed9c3481944cdc029b5f4bc7fb0634f732a
Luther <luther0211@hotmail.com>
2019-02-13 23:15:26 -0600

    Getting input value

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,18 @@
 import Axios from "axios";
 
+import { elements } from "./views/base"
+
 const state = {
 
 };
+
+elements.searchForm.addEventListener("submit", e => {
+  e.preventDefault()
+  console.log(elements.searchInput.value)
+})
+
+
+
 
 const init = async () => {
   state.DATA = await Axios("https://pokeapi.co/api/v2/pokemon/?limit=964").then(data => data.data.results)
