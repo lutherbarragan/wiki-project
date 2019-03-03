afe593553bfa9ade03d89e2cf1fc8f837afd24c1
Luther <luther0211@hotmail.com>
2019-03-03 14:16:31 -0600

    added method to sort the sprite images

--- src/js/models/SelectedResult.js
+++ src/js/models/SelectedResult.js
@@ -8,7 +8,33 @@ export default class SelectedResult {
 
   getSprites() {
     let urls = Object.values(this.data.sprites).filter(cur => cur !== null)
-    return urls
+    let normal = [];
+    let shiny = [];
+    let female = [];
+    
+    urls.forEach(link => {
+      if(!link.includes("shiny") && !link.includes("back") && !link.includes("female")) { // Normal
+        normal.push(link)
+      } else if(!link.includes("shiny") && link.includes("back") && !link.includes("female")) { // Normal back sprite
+        normal.push(link)
+      } else if(link.includes("shiny") && !link.includes("back") && !link.includes("female")) { // Shiny sprite
+        shiny.push(link)
+      }else if(link.includes("shiny") && link.includes("back") && !link.includes("female")) { //Shiny back sprite
+        shiny.push(link)
+      }else if(!link.includes("shiny") && !link.includes("back") && link.includes("female")) { // Female sprite
+        female.push(link)        
+      }else if(link.includes("shiny") && !link.includes("back") && link.includes("female")) { // Shiny female sprite
+        female.push(link)  
+      }
+    })
+    
+    normal.sort(function(a, b){return a.length - b.length})
+    shiny.sort(function(a, b){return a.length - b.length})
+    female.sort(function(a, b){return a.length - b.length})
+    
+    normal = normal.concat(shiny, female)
+
+    return normal
   }
 
 }
