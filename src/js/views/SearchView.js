6b07b84bc4287284d3c21187e0a95362a35629bb
Luther <luther0211@hotmail.com>
2019-03-14 22:19:59 -0600

    added custom alert fro invalid search

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -1,10 +1,11 @@
+import swal from 'sweetalert';
 import { elements } from "./base";
 
 export const getInput = () => {
   if(elements.searchInput.value.length > 2) {
     return elements.searchInput.value
   } else {
-    alert("write at least 3 characters!")
+    swal("Invalid search!", "write at least 3 characters", "error");
     return null
   }
 }
